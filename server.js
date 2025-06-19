10febc72333e9cd45f90ff5b8467c18db2105016
Fay-chebby <chebetf034@gmail.com>
2025-06-19 21:29:15 +0300

    daily task app

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
+
+dotenv.config();
+connectDB();
+
 const app = express();
-const PORT = 3000;
+app.use(cors());
+app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello");
 });
+
+const PORT = process.envPORT || 3000;
 app.listen(PORT, () => {
   console.log(`Listening port on ${PORT}`);
 });
